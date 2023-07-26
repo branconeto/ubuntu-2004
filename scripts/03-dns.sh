66d1a4cf4d100a47ca208b84116d8815bbc44e60
Salvador Branco <sbninformatica@gmail.com>
2023-07-26 13:16:38 -0300

    Alteracao

--- scripts/03-dns.sh
+++ scripts/03-dns.sh
@@ -227,12 +227,12 @@ echo -e "Atualizando os arquivos de configuração do Bind DNS Server, aguarde..
 	mv -v /etc/bind/rndc.key /etc/bind/rndc.key.old &>> $LOG
 	mv -v /etc/default/named /etc/default/named.old &>> $LOG
 	cp -v conf/dns/{named.conf,named.conf.local,named.conf.options,named.conf.default-zones,rndc.key} /etc/bind/ &>> $LOG
-	cp -v conf/dns/{pti.intra.hosts,172.16.1.rev} /var/lib/bind/ &>> $LOG
+	cp -v conf/dns/{sbn.local.hosts,192.168.1.rev} /var/lib/bind/ &>> $LOG
 	cp -v conf/dns/{dnsupdate-cron,rndcupdate-cron} /etc/cron.d/ &>> $LOG
 	cp -v conf/dns/named /etc/default/ &>> $LOG
 	cp -v conf/dns/rndcstats /etc/logrotate.d/ &>> $LOG
 	chown -v root:bind /etc/bind/rndc.key &>> $LOG
-	chown -v root:bind /var/lib/bind/{pti.intra.hosts,172.16.1.rev} &>> $LOG
+	chown -v root:bind /var/lib/bind/{sbn.local.hosts,192.168.1.rev} &>> $LOG
 echo -e "Arquivos atualizados com sucesso!!!, continuando com o script...\n"
 sleep 5
 #
@@ -274,22 +274,22 @@ echo -e "Editando o arquivo de configuração rndc.key, pressione <Enter> para c
 echo -e "Arquivo editado com sucesso!!!, continuando com o script...\n"
 sleep 5
 #
-echo -e "Editando o arquivo de configuração pti.intra.hosts, pressione <Enter> para continuar."
+echo -e "Editando o arquivo de configuração sbn.local.hosts, pressione <Enter> para continuar."
 	# opção do comando: &>> (redirecionar a saída padrão)
 	# opção do comando read: -s (Do not echo keystrokes)
 	read -s
-	vim /var/lib/bind/pti.intra.hosts
-	named-checkzone $DOMAIN /var/lib/bind/pti.intra.hosts &>> $LOG
+	vim /var/lib/bind/sbn.local.hosts
+	named-checkzone $DOMAIN /var/lib/bind/sbn.local.hosts &>> $LOG
 echo -e "Arquivo editado com sucesso!!!, continuando com o script...\n"
 sleep 5
 #
-echo -e "Editando o arquivo de configuração 172.16.1.rev, pressione <Enter> para continuar."
+echo -e "Editando o arquivo de configuração 192.168.1.rev, pressione <Enter> para continuar."
 	# opção do comando: &>> (redirecionar a saída padrão)
 	# opção do comando read: -s (Do not echo keystrokes)
 	read -s
-	vim /var/lib/bind/172.16.1.rev
-	named-checkzone $DOMAINREV /var/lib/bind/172.16.1.rev &>> $LOG
-	named-checkzone $NETWORK /var/lib/bind/172.16.1.rev &>> $LOG
+	vim /var/lib/bind/192.168.1.rev
+	named-checkzone $DOMAINREV /var/lib/bind/192.168.1.rev &>> $LOG
+	named-checkzone $NETWORK /var/lib/bind/192.168.1.rev &>> $LOG
 echo -e "Arquivo editado com sucesso!!!, continuando com o script...\n"
 sleep 5
 #
